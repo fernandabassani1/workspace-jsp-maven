599250d661f2a49f16e7f98d212e0ea2b20befb1
fernandabassani1 <fernandabassani1@gmail.com>
2019-06-03 16:40:48 -0300

    acrescentando atributos e no parametro do getParameter é inserido o atributo p/ >> (o link "expressao" recuperar esse atributo.)
    é dada um new Cliente no parametro do service.criarCliente e passando os atributos delA, esse new Cliente só possivel usalá diretamente pois é possuido um construtor proprio "model"

--- fastvendas/src/main/java/br/com/sisvenda/controller/cliente/CadastraClienteServlet.java
+++ fastvendas/src/main/java/br/com/sisvenda/controller/cliente/CadastraClienteServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import br.com.sisvenda.model.Cliente;
 import br.com.sisvenda.service.ClienteService;
 
 
@@ -23,21 +24,21 @@ public class CadastraClienteServlet extends HttpServlet {
 		// trazer um objeto do html  no getParamiter
 		
 		String nome = request.getParameter("nome");
+		String sobrenome = request.getParameter("sobrenome");
+		String email = request.getParameter("email");
 		String documento = request.getParameter("documento");
-	
+		String telefone = request.getParameter("telefone");
+		String endereco = request.getParameter("endereco");
+		String cep = request.getParameter("cep");
+		String senha = request.getParameter("senha");
 		
+	
 		//instanciar o servico de cliente e chamar o criarCliente, passando o atributo no parametro.
 		
 		ClienteService service = new ClienteService();
-		service.criarCliente(nome, documento);
-		
-		
-		// manda msg na tela web .
-		String msg = "cliente " + nome + " salvo com sucesso.";
-		HttpSession session = request.getSession();
-		session.setAttribute("msg", msg);
-		
-		response.sendRedirect("listaCliente");
+		service.criarCliente(new Cliente(null,nome, sobrenome, email, documento, telefone, endereco, cep, senha));
+				
+		response.sendRedirect("cadastraClienteForm");
 		
 		
 	}
