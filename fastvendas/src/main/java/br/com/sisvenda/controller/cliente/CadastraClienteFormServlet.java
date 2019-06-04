08b7cdb554816abe3e59437e9e7dbd61edd2b0bd
fernandabassani1 <fernandabassani1@gmail.com>
2019-06-04 12:46:29 -0300

    mandando para jsp

--- fastvendas/src/main/java/br/com/sisvenda/controller/cliente/CadastraClienteFormServlet.java
+++ fastvendas/src/main/java/br/com/sisvenda/controller/cliente/CadastraClienteFormServlet.java
@@ -13,7 +13,7 @@ public class CadastraClienteFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("listaCliente");
+		response.sendRedirect("cadastraCliente.jsp");
 	}
 
 }
